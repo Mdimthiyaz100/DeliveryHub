65105bab5b98ebe31284e7c93d6b7c68cc6daf91
Mohamed imthiyaz <mimthiyaz100@gmail.com>
2026-08-16 15:17:07 +0530

    use aiven database url

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+    uri: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
 async function query(sql, params) {
     const [rows] = await pool.execute(sql, params);
